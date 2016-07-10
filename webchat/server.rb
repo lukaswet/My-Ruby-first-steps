ab381e4684488fcd4d03fc0825288ae87725e3c5
lukaswet <lukaswet@gmail.com>
2016-07-10 16:09:07 +0300

    testing middleman

--- webchat/server.rb
+++ webchat/server.rb
@@ -7,18 +7,39 @@ class ChatConnection < EventMachine::WebSocket::Connection
 				super
 				onopen { on_open }
 				onmessage { |message| on_message(message) }
+				onclose { on_close }
 		end
 
 		def on_open
-				puts 'new connection created'
+				Chat.add_connection self
 		end
 
 		def on_message(message)
 				puts message
 		end
 
+		def on_close
+				Chat.delete_connection self
+		end
+
 end	
 
+module Chat
+		CONNECTION = []
+
+		module_function
+
+		def add_connection(connection)
+				CONNECTION.push connection
+				puts 'new connection created'
+
+		end
+
+		def delete_connection(connection)
+				CONNECTION.delete connection
+				puts 'connection closed'				
+		end	
+end
 
 
 EM.run do 
