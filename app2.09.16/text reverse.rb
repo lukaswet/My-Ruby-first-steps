b2c976eefb7cc85d83c30acee1ab28ce3e933906
Lukas <lukaswet@gmail.com>
2016-08-27 17:29:21 +0300

    Reverse words and counting words apps practice

---
+++ app2.09.16/text reverse.rb
@@ -0,0 +1,6 @@
+s = "Officia pariatur non irure ad commodo pariatur tempor excepteur sunt mollit aliqua ut. 
+Lorem do ut ullamco fugiat nisi qui fugiat esse ullamco tempor mollit qui. 
+Qui Lorem quis nulla proident labore ad irure eiusmod labore aliqua veniam dolor et sit. 
+Laboris commodo proident voluptate ipsum cillum mollit officia velit ut amet cillum sint."
+
+puts s.split(/ /).reverse.join(' ')
